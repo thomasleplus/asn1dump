3c223a7c880346a611c3a1bc97e5c21956bf74c2
Thomas Leplus <thomasleplus@users.noreply.github.com>
2025-05-06 19:27:58 +0200

    Add tests

--- src/main/java/org/leplus/asn1dump/Main.java
+++ src/main/java/org/leplus/asn1dump/Main.java
@@ -1,10 +1,7 @@
 package org.leplus.asn1dump;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import java.io.*;
 import org.bouncycastle.asn1.ASN1InputStream;
 import org.bouncycastle.asn1.ASN1Primitive;
 import org.bouncycastle.asn1.util.ASN1Dump;
@@ -15,12 +12,17 @@ public final class Main {
   @SuppressFBWarnings("PATH_TRAVERSAL_IN")
   public static void main(final String[] args) throws IOException {
     for (final String arg : args) {
-      try (FileInputStream fis = new FileInputStream(new File(arg));
-          ASN1InputStream asn1InputStream = new ASN1InputStream(fis)) {
-        ASN1Primitive obj;
-        while ((obj = asn1InputStream.readObject()) != null) {
-          System.out.println(ASN1Dump.dumpAsString(obj, true));
-        }
+      try (FileInputStream in = new FileInputStream(new File(arg))) {
+        readAndPrint(in, System.out);
+      }
+    }
+  }
+
+  public static void readAndPrint(final InputStream in, final PrintStream out) throws IOException {
+    try (ASN1InputStream asn1InputStream = new ASN1InputStream(in)) {
+      ASN1Primitive obj;
+      while ((obj = asn1InputStream.readObject()) != null) {
+        out.println(ASN1Dump.dumpAsString(obj, true));
       }
     }
   }
---
+++ src/test/java/org/leplus/asn1dump/MainTest.java
@@ -0,0 +1,33 @@
+package org.leplus.asn1dump;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+  public static final String INPUT_1 = "src/test/resources/sample1.der";
+  public static final String INTPUT_2 = "src/test/resources/sample2.der";
+  public static final String OUTPUT = "src/test/resources/samples.txt";
+
+  @Test
+  void readAndPrint1() throws IOException {
+    final byte[] expected = Files.readAllBytes(Paths.get(OUTPUT));
+    final ByteArrayOutputStream out = new ByteArrayOutputStream();
+    Main.readAndPrint(new FileInputStream(INPUT_1), new PrintStream(out));
+    Main.readAndPrint(new FileInputStream(INTPUT_2), new PrintStream(out));
+    Assertions.assertArrayEquals(expected, out.toByteArray());
+  }
+
+  @Test
+  void readAndPrint2() throws IOException {
+    final byte[] expected = Files.readAllBytes(Paths.get(OUTPUT));
+    final ByteArrayOutputStream out = new ByteArrayOutputStream();
+    Main.readAndPrint(
+        new SequenceInputStream(new FileInputStream(INPUT_1), new FileInputStream(INTPUT_2)),
+        new PrintStream(out));
+    Assertions.assertArrayEquals(expected, out.toByteArray());
+  }
+}
